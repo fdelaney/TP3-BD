bfb367abe4164831c79fb94ce0ee78b8bfea51a0
Francois Chantal <geniousmouton@gmail.com>
2015-04-29 15:46:27 -0400

    autre ajout encore

--- Tutorat/DataLayer/EfEntityFramework/EfDataBaseHelper.cs
+++ Tutorat/DataLayer/EfEntityFramework/EfDataBaseHelper.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataLayer.Model;
 
 namespace DataLayer.EfEntityFramework
 {
     class EfDataBaseHelper
     {
+        private EfEntityRepository<Tutor> _tutorRepository;
+        private EfEntityRepository<TutoringSession> _tutoringSessionRepository;
+        private EfEntityRepository<HelpedStudent> _helpedStudentRepository;
     }
 }
--- Tutorat/DataLayer/Model/TutoringSession.cs
+++ Tutorat/DataLayer/Model/TutoringSession.cs
@@ -2,5 +2,7 @@
 {
     public class TutoringSession : Entity
     {
+        public int HelpedId { get; set; }
+        public int TutorId { get; set; }
     }
 }
