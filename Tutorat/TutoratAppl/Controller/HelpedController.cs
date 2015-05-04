35b0eab92ba6fb096690e316d9ed1df9c79fb096
Francois Chantal <geniousmouton@gmail.com>
2015-05-04 14:30:46 -0400

    fichier pas inclue oups

---
+++ Tutorat/TutoratAppl/Controller/HelpedController.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using DataLayer.Model;
+
+namespace TutoratAppl.Controller
+{
+    public class HelpedController
+    {
+        private readonly IEntityRepository<HelpedStudent> _helpedRepo;
+
+        public HelpedController(IEntityRepository<HelpedStudent> _repository)
+        {
+            _helpedRepo = _repository;
+        }
+
+        public void ListAll()
+        {
+        }
+
+        public void ListAllWhenWithoutTutoringSession()
+        {
+        }
+    }
+}
